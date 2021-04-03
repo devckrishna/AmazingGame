0a554ade3c10e96de387eb7bd7913a9abbdff3d8
devckrishna <dev.c.krishna.123@gmail.com>
2021-04-03 20:29:45 +0530

    fc

---
+++ Assets/scripts/CoinSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawner : MonoBehaviour
+{
+    public float timeBetweenSpawns;
+    public GameObject prefab;
+
+    private float timeToNextSpawn;
+
+    void Start()
+    {
+        timeToNextSpawn=timeBetweenSpawns;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeToNextSpawn-=Time.deltaTime;
+        if(timeToNextSpawn <= 0.0f){
+            
+            Debug.Log("Time is up!!!");
+            timeToNextSpawn=timeBetweenSpawns;
+        }
+        
+    }
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    public float jumpsAllowed;
+
+    private float timesJumped;
+    private Rigidbody2D rb;
+    private float moveInput;
+    private float jumpInput;
+    private float previousJumpInput;
+
+    void Start()
+    {
+        rb=GetComponent<Rigidbody2D>();
+        timesJumped=0;
+        previousJumpInput=0;
+    }
+
+    void FixedUpdate(){
+        moveInput=Input.GetAxis("Horizontal");
+        rb.velocity=new Vector2(moveInput*speed,rb.velocity.y);
+
+        jumpInput=Input.GetAxis("Vertical");
+        if(jumpInput > 0 && timesJumped < jumpsAllowed && previousJumpInput==0){
+            rb.velocity=new Vector2(rb.velocity.x,jumpForce);
+            timesJumped+=1;
+        }
+        previousJumpInput=jumpInput;
+    }
+
+    void OnCollisionEnter2D(Collision2D other){
+        if(other.gameObject.CompareTag("Ground")){
+            timesJumped=0;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
